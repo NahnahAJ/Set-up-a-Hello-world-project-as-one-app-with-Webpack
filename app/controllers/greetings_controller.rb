dd0006cd15645a9576359bbe9a9735c892d1f136
nahnahAJ <nahnahahjuwah@gmail.com>
2023-02-20 21:53:43 +0000

    Generate API endpoint  for greetings

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,2 +1,4 @@
 class GreetingsController < ApplicationController
+  def index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :greetings, only: [:index]
+    end
+  end
+
   resources :greetings
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "greetings#index"
 end
