3a572b322ab8896f5bc61347b5bcbb7d30c15e08
nahnahAJ <nahnahahjuwah@gmail.com>
2023-02-22 16:44:34 +0000

    Create the App component

---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const App = () => {
+  <>
+      <h2>Link to greeting</h2>
+  <Link to='/greeting'>Greeting component</Link>
+  </>
+    
+}
+
+export default App;
