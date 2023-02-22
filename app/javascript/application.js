9504d1c8c80a23ffe39a2b7e059c49452da8810e
nahnahAJ <nahnahahjuwah@gmail.com>
2023-02-22 16:43:52 +0000

    Add routers for the application

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,30 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import '@hotwired/turbo-rails';
+import './controllers';
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './components/App';
+import Greeting from './components/Greeting';
+import store from './store';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/greeting',
+    element: <Greeting />,
+  },
+]);
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
